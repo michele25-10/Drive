6c80bd8293fa9fd9b859a3f7961eb5dfaa0f08d9
michele25-10 <michele.gabri01@gmail.com>
2023-09-17 10:09:17 +0200

    Terminata configurazione firebase
    Fatta anche la function uploadFile.tsx

--- clone-2.0/src/components/Home/index.tsx
+++ clone-2.0/src/components/Home/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import TopBar from "../TopBar/index";
-import { signIn, signOut } from "next-auth/react";
-import Button from "@/components/common/Button/Button";
-import styles from "./Home.module.scss";
-import UploadFiles from "../uploadFiles";
+import UploadFiles from "@/components/uploadFiles/index";
 
 export default function HomeComponent() {
   return (
--- clone-2.0/src/components/uploadFiles/index.tsx
+++ clone-2.0/src/components/uploadFiles/index.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import styles from "./uploadFiles.module.scss";
 import Button from "../common/Button/Button";
+import { fileUpload } from "@/API/FileUpload";
 
 export default function UploadFiles() {
+  const [isFileVisible, setFileVisible] = useState(false);
+  const [file, setFile] = useState({});
+  const uploadFiles = async (event: ChangeEvent<HTMLInputElement>) => {
+    let file = event.target.files?.[0];
+    fileUpload(file);
+  };
   return (
     <div className={styles.uploadMain}>
-      <Button title="Aggiungi file" btnClass="btn-primary" />
-
+      <Button
+        onClick={() => setFileVisible(!isFileVisible)}
+        title="Aggiungi file"
+        btnClass="btn-info"
+      />
+      {isFileVisible ? (
+        <input
+          type="file"
+          className="file-input file-input-bordered file-input-info w-full max-w-xs"
+          onChange={(event) => uploadFiles(event)}
+        />
+      ) : (
+        <></>
+      )}
       <Button title="Aggiungi cartella" btnClass="btn-primary btn-outline" />
     </div>
   );
