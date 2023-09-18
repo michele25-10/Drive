a0a593f1bf8e89a583239590288226900afe4583
michele25-10 <michele.gabri01@gmail.com>
2023-09-18 18:47:20 +0200

    Fatta function addFolder
    Ti permette di inserire dentro allo storage una cartella

--- clone-2.0/src/API/FireStore.tsx
+++ clone-2.0/src/API/FireStore.tsx
@@ -11,6 +11,24 @@ export const addFiles = (imageLink: string, imageName: string) => {
     addDoc(files, {
       imageLink: imageLink,
       imageName: imageName,
+      isFolder: false,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const addFolder = (payload: {
+  folderName: string;
+  isFolder: boolean;
+  fileList: object;
+}) => {
+  try {
+    //Dove effettivamente viene fatta la post dei nostri dati insieme al nome della raccolta e ai riferimenti al database
+    addDoc(files, {
+      folderName: payload.folderName,
+      isFolder: payload.isFolder,
+      fileList: payload.fileList,
     });
   } catch (err) {
     console.log(err);
--- clone-2.0/src/components/uploadFiles/index.tsx
+++ clone-2.0/src/components/uploadFiles/index.tsx
@@ -3,14 +3,28 @@ import styles from "./uploadFiles.module.scss";
 import Button from "../common/Button/Button";
 import { fileUpload } from "../../API/FileUpload";
 import CommonProgress from "../common/Progress";
+import { addFolder } from "@/API/FireStore";
 
 export default function UploadFiles() {
   const [isFileVisible, setFileVisible] = useState(false);
+  const [isFolderVisible, setFolderVisible] = useState(false);
   const [progress, setProgress] = useState(0);
+  const [folderName, setFolderName] = useState("");
+
   const uploadFiles = async (event: ChangeEvent<HTMLInputElement>) => {
     let file = event.target.files?.[0];
     fileUpload(file, setProgress);
   };
+
+  const uploadFolder = () => {
+    let payload = {
+      folderName: folderName,
+      isFolder: true,
+      fileList: [],
+    };
+    addFolder(payload);
+  };
+
   return (
     <div className={styles.uploadMain}>
       <Button
@@ -27,7 +41,25 @@ export default function UploadFiles() {
       ) : (
         <></>
       )}
-      <Button title="Aggiungi cartella" btnClass="btn-primary btn-outline" />
+      <Button
+        title="Aggiungi cartella"
+        btnClass="btn-primary btn-outline"
+        onClick={() => setFolderVisible(!isFolderVisible)}
+      />
+      {isFolderVisible ? (
+        <>
+          <input
+            type="text"
+            placeholder="Nome della cartella"
+            onChange={(event) => setFolderName(event.target.value)}
+            className="input input-bordered input-primary w-full max-w-xs"
+          />
+          <Button title="Invia" btnClass="btn-primary" onClick={uploadFolder} />
+        </>
+      ) : (
+        <></>
+      )}
+
       {progress === 0 || progress === 100 ? (
         <></>
       ) : (
