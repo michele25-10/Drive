bfd986282ba65d6a43bd18e8dffba171d445d5c7
michele25-10 <michele.gabri01@gmail.com>
2023-09-18 19:25:59 +0200

    creata la rotta folder

--- clone-2.0/src/components/showFiles/index.tsx
+++ clone-2.0/src/components/showFiles/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import styles from "./ShowFiles.module.scss";
 import { fetchFiles } from "@/hooks/fetchFiles";
 import { AiFillFile, AiFillFolder } from "react-icons/ai";
+import { useRouter } from "next/router";
 
 export default function ShowFiles() {
   let { fileList } = fetchFiles();
+  const router = useRouter();
   const openFile = (fileLink: string) => {
     window.open(fileLink);
   };
@@ -19,25 +21,30 @@ export default function ShowFiles() {
           folderName: "";
         }) => {
           return (
-            <div>
-              <div
-                key={file.id}
-                className={`${styles.files} bg-green-500`}
-                onClick={() => openFile(file.imageLink)}
-              >
-                {file.isFolder ? (
-                  <>
+            <div key={file.id}>
+              {file.isFolder ? (
+                <>
+                  <div
+                    className={styles.files}
+                    onClick={() => router.push(`/folder?id=${file.id}`)} //mi permette di aprire il file in pages [...folder].tsx
+                  >
                     <AiFillFolder size={80} />
                     <p>{file.folderName}</p>
-                  </>
-                ) : (
-                  <>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div
+                    key={file.id}
+                    className={`${styles.files} bg-green-500`}
+                    onClick={() => openFile(file.imageLink)}
+                  >
                     <img className={styles.imageLink} src={file.imageLink} />
                     {/*<AiFillFile size={80} />*/}
                     <p>{file.imageName}</p>
-                  </>
-                )}
-              </div>
+                  </div>
+                </>
+              )}
             </div>
           );
         },
